34a473e30fe01517821e7ba83ab35e2e047eeaa6
fabricio@maxhospedagem.com.br <fabricio@maxhospedagem.com.br>
2016-08-12 17:46:39 -0300

    first commit

---
+++ Assets/Inimigo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Inimigo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        NavMeshAgent agente = GetComponent<NavMeshAgent>();
+        GameObject fimDoCaminho = GameObject.Find("FimDoCaminho");
+        Vector3 vetorFimDoCaminho = fimDoCaminho.transform.position;
+        agente.SetDestination(vetorFimDoCaminho);
+	}
+	
+
+}
